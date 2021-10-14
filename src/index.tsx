4fce1319175b2d24c16ae02ea5bbdfcc0ede57bf
Brenda <brendaprofiro@gmail.com>
2021-10-14 19:08:33 -0300

    chore: Create fake api with miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,66 @@
-import React from 'react';
+import { StrictMode } from 'react';
+
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createServer, Model } from 'miragejs';
+import { v4 as uuidv4 } from 'uuid';
+import { AppProviders } from '@contexts';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 09:00:00'),
+        },
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    });
+
+    this.post('/sessions', (_, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return { token: uuidv4(), user: { id: uuidv4(), name: data.name } };
+    });
+  },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StrictMode>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </StrictMode>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
