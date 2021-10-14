1dcb5a0560f4b2a51ec9d59d87983e51edfe4065
Brenda <brendaprofiro@gmail.com>
2021-10-14 19:00:17 -0300

    chore: Add icons

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as CloseIcon } from './close.svg';
+export { default as DtMoneyIcon } from './dt-money.svg';
+export { default as LogoIcon } from './logo.svg';
+export { default as IncomeIcon } from './income.svg';
+export { default as OutcomeIcon } from './outcome.svg';
+export { default as TotalIcon } from './total.svg';
